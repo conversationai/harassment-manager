14270012b759f83c57aa9fb56108e07a0ea279a3
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-02-24 16:45:06 +0300

    Fixed unnecessary spacing

--- src/app/filter_utils.ts
+++ src/app/filter_utils.ts
@@ -142,7 +142,7 @@ const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
 // differently. We're also interpreting weeks and months relative to today, 
 // but these could also be determined based on where we are in the month.
 // Added an optional filterType parameter to allow for the option of using the current time as the start time instead of midnight.
-export function buildDateFilterForNDays(now: Date, days: number,fromFilter:DayFilterType=DayFilterType.MIDNIGHT): DateFilter {
+export function buildDateFilterForNDays(now: Date, days: number, fromFilter:DayFilterType=DayFilterType.MIDNIGHT): DateFilter {
 
   const fromTime = fromFilter === DayFilterType.NOW  ? now.getTime() : new Date(now).setHours(0, 0, 0, 0);
 
